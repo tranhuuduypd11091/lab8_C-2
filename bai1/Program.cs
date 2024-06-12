f7a42f6c35bb131e62ac833092390ae87b985c22
tranhuuduypd11091 <12345duytran@gmail.com>
2024-06-12 18:05:04 +0700

    lab8

---
+++ bai1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+     class Program
+    {
+        static Random randon = new Random();
+        static object lockObj = new object();
+        static int randomNumber;
+        static bool comleted = false;
+        static void Thread1()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                lock (lockObj)
+                {
+                    randomNumber = randon.Next(1, 11);
+                    Console.WriteLine($"Thread1 : sinh so ngau nhien :{randomNumber}");
+                    Monitor.Pulse(lockObj);
+                    Monitor.Wait(lockObj);
+                }
+                Thread.Sleep(2000);
+
+            }
+            comleted = true;
+        }
+        static void Thread2()
+        {
+            for (int i = 0; i < 10; i++) 
+            {
+                lock (lockObj)
+                {
+                    Monitor.Wait(lockObj);
+                    int squaredNumber = randomNumber * randomNumber;
+                    Console.WriteLine($"Thread2 : Binh phuong cua so : {squaredNumber}");
+                    Monitor.Pulse(lockObj);
+                }
+                Thread.Sleep(1000);
+            }
+
+        }
+        static void Main()
+        {
+            Thread thread1 = new Thread(Thread1);
+            Thread thread2 = new Thread(Thread2);
+
+            thread1.Start();
+            thread2.Start();
+            while (!comleted)
+            {
+                Thread.Sleep(1000);
+            }
+            Console.WriteLine("Ket thuc chuong trinh");
+        }
+    }
+}
---
+++ bai2/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace bai2
+{
+    public class Program
+    {
+        static object lock1 = new object();
+        static object lock2 = new object();
+
+        public static void Thread1FunctionLab8()
+        {
+            if (Monitor.TryEnter(lock1, TimeSpan.FromMilliseconds(1000)))
+            {
+                try
+                {
+                    Console.WriteLine("Thread 1 da khoa lock1");
+                    Thread.Sleep(100);
+
+                    if (Monitor.TryEnter(lock2, TimeSpan.FromMilliseconds(1000)))
+                    {
+                        try
+                        {
+                            Console.WriteLine("Thread 1 da khoa lock2");
+                        }
+                        finally
+                        {
+                            Monitor.Exit(lock2);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Thread 1 khong the khoa lock2");
+                    }
+                }
+                finally
+                {
+                    Monitor.Exit(lock1);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Thread 1 khong the khoa lock1");
+            }
+        }
+
+        public static void Thread2FunctionLab8()
+        {
+            if (Monitor.TryEnter(lock2, TimeSpan.FromMilliseconds(1000)))
+            {
+                try
+                {
+                    Console.WriteLine("Thread 2 da khoa lock2");
+                    Thread.Sleep(100);
+
+                    if (Monitor.TryEnter(lock1, TimeSpan.FromMilliseconds(1000)))
+                    {
+                        try
+                        {
+                            Console.WriteLine("Thread 2 da khoa lock1");
+                        }
+                        finally
+                        {
+                            Monitor.Exit(lock1);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Thread 2 khong the khoa lock1");
+                    }
+                }
+                finally
+                {
+                    Monitor.Exit(lock2);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Thread 2 khong the khoa lock2");
+            }
+        }
+
+        public static void Main()
+        {
+            Thread thread1 = new Thread(Thread1FunctionLab8);
+            Thread thread2 = new Thread(Thread2FunctionLab8);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+            Console.WriteLine("Ket thuc threads");
+        }
+    }
+}
+
